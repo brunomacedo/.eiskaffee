8f86f77579df3a078bfb9845407b74e2fe4228f6
Bruno Macedo <contato@brunomacedo.com.br>
2018-08-04 17:03:02 -0300

    [BASH] add OS function

--- eiskaffee.sh
+++ eiskaffee.sh
@@ -1,6 +1,7 @@
 # Imports
 source "${FINDROOT}/lib/colors.eisk"
-source ${FINDROOT}/lib/alias.eisk
-source ${FINDROOT}/lib/git.eisk
-source ${FINDROOT}/lib/npm.eisk
-source ${FINDROOT}/tools/rename.eisk
+source "${FINDROOT}/lib/alias.eisk"
+source "${FINDROOT}/lib/git.eisk"
+source "${FINDROOT}/lib/npm.eisk"
+source "${FINDROOT}/tools/rename.eisk"
+source "${FINDROOT}/tools/system.eisk"
