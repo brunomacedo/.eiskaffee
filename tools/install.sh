f54dd5e53034889b39de2e348c2bec4582fa49d3
Bruno Macedo <contato@brunomacedo.com.br>
2018-08-06 15:20:40 -0300

    [BASH] install from raw

--- tools/install.sh
+++ tools/install.sh
@@ -1,18 +1,20 @@
-source "../lib/colors.eisk"
+source ~/.eiskaffee/lib/colors.eisk
 
-if [ ! -f ~/.bash_profile ]; then
+if [ ! -f ~/.eiskrc ]; then
+  if [ -f ~/.eiskaffee/tools/.eiskrc ]; then
+    cp ~/.eiskaffee/tools/.eiskrc ~/.eiskrc
+  else
+    printf "${redb}"
+    echo "No such file or directory."
+    printf "${end}"
+  fi
+fi
+
+if [ ! -f ~/.bash_profile  ]; then
   touch ~/.bash_profile
 fi
 
-if ! grep -Fxq "source ~/.eiskrc" ~/.bash_profile > /dev/null; then
-  if [ ! -f ~/.eiskrc ]; then
-    if [ -f .eiskrc ]; then
-      cp .eiskrc ~/.eiskrc
-    else
-      echo -e "${redb}No such file or directory.${end}"
-    fi
-  fi
-
+if ! grep -Fxq "source ~/.eiskrc" ~/.bash_profile  > /dev/null; then
   echo "source ~/.eiskrc" >> ~/.bash_profile
 
   printf "${greenb}"
@@ -29,5 +31,7 @@ if ! grep -Fxq "source ~/.eiskrc" ~/.bash_profile > /dev/null; then
   echo ""
   printf "${end}"
 else
-  echo -e "${greenb}Eiskaffee already has been installed!${end}"
+  printf "${greenb}"
+  echo "You already have Eiskaffee installed."
+  printf "${end}"
 fi
