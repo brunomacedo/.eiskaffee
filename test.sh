de14a929ec39b18e712a1caf8effcc178e551366
Bruno Macedo <contato@brunomacedo.com.br>
2018-08-10 11:28:31 -0300

    reindent lines

--- test.sh
+++ test.sh
@@ -33,19 +33,19 @@ assert_equals() {
 # Generate the list of tests to run.
 main() {
 
-    head="-> Running tests on the ${blueb}Eiskaffee...${end}"
-    printf '\n%s\n%s\n' "$head" "${head//?/-}"
+  head="-> Running tests on the ${blueb}Eiskaffee...${end}"
+  printf '\n%s\n%s\n' "$head" "${head//?/-}"
 
-    IFS=$'\n'
-    for func in $(declare -F); do
-      [[ "${func:11}" == test_* ]] && "${func:11}";
-    done
+  IFS=$'\n'
+  for func in $(declare -F); do
+    [[ "${func:11}" == test_* ]] && "${func:11}";
+  done
 
-    comp="Completed $((fail+pass)) tests. ${greenb}${pass:-0} passed${end}, ${redb}${fail:-0} failed.${end}"
-    printf '%s\n%s\n\n' "${comp//?/-}" "$comp"
+  comp="Completed $((fail+pass)) tests. ${greenb}${pass:-0} passed${end}, ${redb}${fail:-0} failed.${end}"
+  printf '%s\n%s\n\n' "${comp//?/-}" "$comp"
 
-    # If a test failed, exit with '1'.
-    ((fail>0)) || exit 0 && exit 1
+  # If a test failed, exit with '1'.
+  ((fail>0)) || exit 0 && exit 1
 }
 
 main "$@"
