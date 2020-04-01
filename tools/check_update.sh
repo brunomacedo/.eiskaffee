ab4851d0357cd3c08d60bb0f7ddbb744e5d5984e
Bruno Macedo <contato@brunomacedo.com.br>
2020-04-01 18:09:00 -0300

    feat: create auto update with confirm

--- tools/check_update.sh
+++ tools/check_update.sh
@@ -5,27 +5,40 @@ __command_exists git || {
   return
 }
 
-git fetch --quiet
-checkRef="$(command git log HEAD..origin --pretty=format:%h 2> /dev/null)"
+__git_run_update() {
+  git pull --quiet
 
-if [[ "$checkRef" ]]; then
-  printf "\nWould you like to update? ${yellowb}[Y/n]${end}: \n"
-  read line
-  if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
-    git pull --quiet
+  printf "${green}"
+  echo ""
+  echo ""
+  echo "▓█████  ██▓  ██████  ██ ▄█▀▄▄▄        █████▒ █████▒▓█████ ▓█████ ";
+  echo "▓█   ▀ ▓██▒▒██    ▒  ██▄█▒▒████▄    ▓██   ▒▓██   ▒ ▓█   ▀ ▓█   ▀ ";
+  echo "▒███   ▒██▒░ ▓██▄   ▓███▄░▒██  ▀█▄  ▒████ ░▒████ ░ ▒███   ▒███   ";
+  echo "▒▓█  ▄ ░██░  ▒   ██▒▓██ █▄░██▄▄▄▄██ ░▓█▒  ░░▓█▒  ░ ▒▓█  ▄ ▒▓█  ▄ ";
+  echo "░▒████▒░██░▒██████▒▒▒██▒ █▄▓█   ▓██▒░▒█░   ░▒█░    ░▒████▒░▒████▒";
+  echo "░░ ▒░ ░░▓  ▒ ▒▓▒ ▒ ░▒ ▒▒ ▓▒▒▒   ▓▒█░ ▒ ░    ▒ ░    ░░ ▒░ ░░░ ▒░ ░";
+  echo "                                                                 ";
+  echo "Eiskaffee has been updated."
+  echo ""
+  printf "${end}"
+}
 
-    printf "${green}"
-    echo ""
-    echo ""
-    echo "▓█████  ██▓  ██████  ██ ▄█▀▄▄▄        █████▒ █████▒▓█████ ▓█████ ";
-    echo "▓█   ▀ ▓██▒▒██    ▒  ██▄█▒▒████▄    ▓██   ▒▓██   ▒ ▓█   ▀ ▓█   ▀ ";
-    echo "▒███   ▒██▒░ ▓██▄   ▓███▄░▒██  ▀█▄  ▒████ ░▒████ ░ ▒███   ▒███   ";
-    echo "▒▓█  ▄ ░██░  ▒   ██▒▓██ █▄░██▄▄▄▄██ ░▓█▒  ░░▓█▒  ░ ▒▓█  ▄ ▒▓█  ▄ ";
-    echo "░▒████▒░██░▒██████▒▒▒██▒ █▄▓█   ▓██▒░▒█░   ░▒█░    ░▒████▒░▒████▒";
-    echo "░░ ▒░ ░░▓  ▒ ▒▓▒ ▒ ░▒ ▒▒ ▓▒▒▒   ▓▒█░ ▒ ░    ▒ ░    ░░ ▒░ ░░░ ▒░ ░";
-    echo "                                                                 ";
-    echo "Eiskaffee has been updated."
-    echo ""
-    printf "${end}"
+__git_check_update() {
+  local CURRENT_DIR="$PWD"
+  cd "$EISK_HOME"
+
+  git fetch --quiet
+  __GIT_CHECK_REF="$(command git log HEAD..origin --pretty=format:%h 2> /dev/null)"
+
+  if [[ "$__GIT_CHECK_REF" ]]; then
+    printf "\nWould you like to update? ${yellowb}[Y/n]${end}: \n"
+    read line
+    if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
+      __git_run_update
+    fi
   fi
-fi
+
+  cd "$CURRENT_DIR"
+}
+
+__git_check_update
